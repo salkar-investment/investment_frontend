93c5343eecc793bc4b05394ba8e4c3ac1d068e6a
Sergey Sokolov <sokolov.sergey.a@gmail.com>
2023-11-26 00:30:02 +0300

    Closes #5 - Setup eslint config

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  );
+
+  expect(screen.getByText(/learn/i)).toBeInTheDocument();
 });
